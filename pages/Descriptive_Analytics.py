6686c8a3a7e22c6f77c90ac2ae46ddefbb9b2745
pamcast <pamela.cast.glez@gmail.com>
2025-09-28 20:45:15 +0200

    Update Descriptive_Analytics.py

--- pages/Descriptive_Analytics.py
+++ pages/Descriptive_Analytics.py
@@ -66,7 +66,7 @@ with tab2:
     with col2:
         # Heart Disease and Stroke
         fig_heart = px.bar(df.groupby(['Heart Disease', "Stroke"]).size().reset_index(name='count'),
-                            x='Heart Disease', y='Stroke', color="#59e698",
+                            x='Heart Disease', y='count', color="Stroke",
                             title='Heart Disease and Stroke',
                             labels={'Heart Disease': 'Heart Disease', 'count': 'Count'})
         fig_heart.update_layout(height=400)
